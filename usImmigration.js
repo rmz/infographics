3a2c5b4f5742f7c8f4b134f65eb51aed6a6c71a8
Ricardo Miranda Zuniga <rmira@hunter.cuny.edu>
2026-04-14 19:46:29 -0400

    Add files via upload

---
+++ usImmigration.js
@@ -0,0 +1,16 @@
+﻿/* This code uses the Intersection Observer API to watch elements on the page and toggle a CSS class based on whether they’re visible in the viewport. */
+//Creates a new observer that runs a callback whenever observed elements enter or leave the viewport.
+//entries is an array of objects describing each observed element’s visibility.
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => { //Loop through observed entries
+        console.log(entry) //Log the entry and its properties to the console for debugging purposes.
+        if (entry.isIntersecting) { //Check if the element is visible
+            entry.target.classList.add('show'); //Add the 'show' class if the element is visible else remove it
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
